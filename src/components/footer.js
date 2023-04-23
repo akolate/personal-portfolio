30c7115982b7f6986068ab18b2bd8af35dd1e323
akolate <aniketkolte18@gmail.com>
2023-04-23 15:39:03 +0530

    Update repository link in footer section

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/pycoder2000/portfolio-v4">
+        <a href="https://github.com/akolate/personal-portfolio/">
           <div>Designed &amp; Built by Brittany Chiang</div>
           <br></br>
           <div>Revised by Aniket Kolte</div>
